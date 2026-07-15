01add1c34c64e6c78dfb1eadadfb2a4b7e4cb2f6
Pasupathiv47 <pasupathiv15@gmail.com>
2026-07-15 14:04:13 +0530

    Implement MainActivity with button navigation

---
+++ app/src/main/java/com/dealerapp/orders/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dealerapp.orders
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnDealers).setOnClickListener {
+            startActivity(Intent(this, DealersActivity::class.java))
+        }
+        findViewById<Button>(R.id.btnItems).setOnClickListener {
+            startActivity(Intent(this, ItemsActivity::class.java))
+        }
+        findViewById<Button>(R.id.btnNewOrder).setOnClickListener {
+            startActivity(Intent(this, NewOrderActivity::class.java))
+        }
+        findViewById<Button>(R.id.btnOrders).setOnClickListener {
+            startActivity(Intent(this, OrdersActivity::class.java))
+        }
+    }
+}
